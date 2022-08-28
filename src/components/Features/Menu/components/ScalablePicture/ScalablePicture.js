5d1ad63090b08af2442429d51f6620744f39b76b
Madalin <turluianu.petre.madalin@gmail.com>
2022-08-28 23:32:17 +0300

    +

--- src/components/Features/Menu/components/ScalablePicture/ScalablePicture.js
+++ src/components/Features/Menu/components/ScalablePicture/ScalablePicture.js
@@ -3,9 +3,14 @@ import styles from "./ScalablePicture.module.css";
 function ScalablePicture({ picture, style }) {
   return (
     <div className={styles.scalablePicture} style={style}>
-      <div>
-        <img src={picture} alt=""/>
+      <div className={styles.voidCyrcle}>
+        <div className={styles.imageWrapper}>
+          <img src={picture} alt="" />
+        </div>
       </div>
+      <div className={styles.void1}></div>
+      <div className={styles.void2}></div>
+      <div className={styles.void3}></div>
     </div>
   );
 }
