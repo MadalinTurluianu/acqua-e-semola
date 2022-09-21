3040ba6cbcb2cd5e32cdb482064b9782578bd2a9
Madalin <turluianu.petre.madalin@gmail.com>
2022-09-21 09:56:59 +0300

    +

--- src/components/Features/Menu/menu-data.js
+++ src/components/Features/Menu/menu-data.js
@@ -1,4 +1,4 @@
-import pastaAlSugoImg from "./assets/pictures/pasta-al-sugo.png";
+import imageUnavailableIcon from "./assets/images/image-unavailable-icon.jpg";
 
 export const menuData = {
   "pasta fresca": [
@@ -7,159 +7,159 @@ export const menuData = {
       description:
         "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci. Con varietà 10€ al kg",
       price: "8.50€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Spaghetti chitarra",
       description:
         "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci. Con varietà 10€ al kg",
       price: "8.50€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Trenette",
       description:
         "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci. Con varietà 10€ al kg",
       price: "8.50€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Pappardelle",
       description:
         "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci. Con varietà 10€ al kg",
       price: "8.50€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Reginette",
       description:
         "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci. Con varietà 10€ al kg",
       price: "8.50€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Fettuccine tagliate a mano",
       price: "10€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Pappardelle tagliate a mano",
       price: "10€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Gnocchi di patate",
       price: "9€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Ravioli alla ricotta",
       price: "16€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Ravioli alla ricotta e spinaci",
       price: "18€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Ravioli alla ricotta e prezzemolo",
       price: "17€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Ravioli alla ricotta e basilico",
       price: "17€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Ravioli alla ricotta con salmone e lime",
       price: "20€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Ravioli con carne",
       price: "18€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Cannelloni di Carne",
       price: "18€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Cannelloni alla ricotta",
       price: "16€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Cannelloni alla ricotta e spinaci",
       price: "17€ al kg",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
   ],
   "piatti pronti": [
     {
       title: "Gnocchi pomodoro e basilico",
       price: "5€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Gnocchi con ragù di verdurine",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Gnocchi pesto e rucola",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Gnocchi con pesto di basilico e mandorle",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Gnocchi con ragù rosso",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Lasagnetta cotta al ragù di verdurine",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Lasagnetta cotta al ragù rosso",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Lasagnetta allo speck e zucchine",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Lasagnetta cotta alla norma",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Cannelloni di ricotta cotti al pomodoro e basilico",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
     {
       title: "Cannelloni di ricotta cotti al sugo rosa",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
   ],
   sughi: [
     {
       title: "Cannelloni di ricotta cotti al sugo rosa",
       price: "6€",
-      picture: pastaAlSugoImg,
+      picture: imageUnavailableIcon,
     },
   ]
 };
