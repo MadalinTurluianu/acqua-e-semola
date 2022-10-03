1de34fbba0855ac31a8d42ce5e89b107d8f73a51
Madalin <turluianu.petre.madalin@gmail.com>
2022-10-03 22:53:39 +0300

    +

--- src/components/Features/Menu/menu-data.js
+++ src/components/Features/Menu/menu-data.js
@@ -24,7 +24,7 @@ export const menuData = {
     {
       title: "Fettuccine",
       description:
-        "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci. Con varietà 10€ al kg",
+        "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci, al basilico. Con varietà 10€ al kg",
       price: "8.50€ al kg",
       icons: [
         fettuccineIcon,
@@ -36,14 +36,14 @@ export const menuData = {
     {
       title: "Spaghetti chitarra",
       description:
-        "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci. Con varietà 10€ al kg",
+        "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci al basilico. Con varietà 10€ al kg",
       price: "8.50€ al kg",
       icons: [imageUnavailableIcon],
     },
     {
       title: "Trenette",
       description:
-        "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci. Con varietà 10€ al kg",
+        "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci al basilico. Con varietà 10€ al kg",
       price: "8.50€ al kg",
       icons: [
         trenettePomodoroIcon,
@@ -56,14 +56,14 @@ export const menuData = {
     {
       title: "Pappardelle",
       description:
-        "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci. Con varietà 10€ al kg",
+        "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci al basilico. Con varietà 10€ al kg",
       price: "8.50€ al kg",
       icons: [imageUnavailableIcon],
     },
     {
       title: "Reginette",
       description:
-        "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci. Con varietà 10€ al kg",
+        "Varietà: al nero di seppia, allo zafferano, al pomodoro, alla rapa rossa, agli spinaci al basilico. Con varietà 10€ al kg",
       price: "8.50€ al kg",
       icons: [reginetteIcon],
     },
@@ -135,60 +135,63 @@ export const menuData = {
   ],
   "piatti pronti": [
     {
-      title: "Gnocchi pomodoro e basilico",
+      title: "Gnocchi di patate",
       price: "5€",
       icons: [imageUnavailableIcon],
     },
     {
-      title: "Gnocchi con ragù di verdurine",
-      price: "6€",
+      title: "Lasagna al ragù rosso pronta a cuocere",
+      price: "13€ al kg",
+      description:
+        "Già cotta a 15€ al kg",
+      icons: [imageUnavailableIcon],
+    },
+
+    {
+      title: "Lasagna verde al ragù rosso pronta a cuocere",
+      price: "13€ al kg",
+      description:
+        "Già cotta a 15€ al kg",
       icons: [imageUnavailableIcon],
     },
     {
-      title: "Gnocchi pesto e rucola",
-      price: "6€",
+      title: "Lasagna zucchine e speck",
+      price: "13€ al kg",
+      description:
+        "Già cotta a 15€ al kg",
       icons: [imageUnavailableIcon],
     },
     {
-      title: "Gnocchi con pesto di basilico e mandorle",
-      price: "6€",
+      title: "Cannelloni di ricotta cotti",
+      price: "18€ al kg",
       icons: [imageUnavailableIcon],
     },
     {
-      title: "Gnocchi con ragù rosso",
-      price: "6€",
+      title: "Cannelloni di carne",
+      price: "20€ al kg",
       icons: [imageUnavailableIcon],
     },
     {
-      title: "Lasagnetta cotta al ragù di verdurine",
-      price: "6€",
-      icons: [imageUnavailableIcon],
-    },
-    {
-      title: "Lasagnetta cotta al ragù rosso",
-      price: "6€",
-      icons: [imageUnavailableIcon],
-    },
-    {
-      title: "Lasagnetta allo speck e zucchine",
-      price: "6€",
-      icons: [imageUnavailableIcon],
-    },
-    {
-      title: "Lasagnetta cotta alla norma",
-      price: "6€",
-      icons: [imageUnavailableIcon],
-    },
-    {
-      title: "Cannelloni di ricotta cotti al pomodoro e basilico",
-      price: "6€",
-      icons: [imageUnavailableIcon],
-    },
-    {
-      title: "Cannelloni di ricotta cotti al sugo rosa",
-      price: "6€",
+      title: "Cannelloni di ricotta e spinaci",
+      price: "20€ al kg",
       icons: [imageUnavailableIcon],
     },
   ],
-  sughi: [],
+  sughi: [
+    {
+      title: "Ragù rosso",
+      price: "15€ al kg",
+      icons: [imageUnavailableIcon],
+    },
+    {
+      title: "Ragù zucchine e speck",
+      price: "14€ al kg",
+      icons: [imageUnavailableIcon],
+    },
+    {
+      title: "Sugo rosso al profumo di basilico",
+      price: "8€ al kg",
+      icons: [imageUnavailableIcon],
+    },
+  ],
 };
