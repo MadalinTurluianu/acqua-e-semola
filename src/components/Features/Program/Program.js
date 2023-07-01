36c0a57c134b1e445ed095bb422d1387ba2ffc47
Madalin <turluianu.petre.madalin@gmail.com>
2023-07-01 13:40:26 +0300

    .

--- src/components/Features/Program/Program.js
+++ src/components/Features/Program/Program.js
@@ -3,9 +3,13 @@ import styles from "./Program.module.css";
 function Program() {
   return (
     <div className={styles.Program}>
-      <p>Martedì - Sabato 09:00-16:30</p>
-      <p>Domenica 09:00-13:00</p>
       <p>Lunedì chiuso</p>
+      <p>Martedì 09:00-14:00</p>
+      <p>Mercoledì 09:00-17:00</p>
+      <p>Giovedì 09:00-14:00</p>
+      <p>Venerdì 09:00-17:00</p>
+      <p>Sabato 09:00-18:30</p>
+      <p>Domenica chiuso</p>
     </div>
   );
 }
